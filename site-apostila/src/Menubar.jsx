51ddee29376e26c14fc6f5db8e6729a6eeba39d9
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2023-08-01 13:42:35 -0300

    aaaa

--- site-apostila/src/Menubar.jsx
+++ site-apostila/src/Menubar.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
 
 
+function menubarSticky(){
+        window.scroll = function() {menubarScroll()};
+        var menubar = document.getElementById("menubar");
+        var sticky = menubar.offsetTop;
 
+        function menubarScroll(){
+          if (window.scrollY >= sticky){
+              menubar.classList.add("sticky");
+          } else {
+              menubar.classList.remove("sticky"); 
+          }
+        }
+      }
+
+export default menubarSticky
