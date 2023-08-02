3cfc1449cbe7459a49138f02d8404a8e9990867a
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2023-08-02 00:12:36 -0300

    aaa

--- site-apostila/src/App.jsx
+++ site-apostila/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css'
-import menubarSticky from './Menubar';
 
 function App() {
   return (
@@ -11,21 +10,72 @@ function App() {
               <div> </div>
               <h3>APOSTILA BÁSICO</h3>            
               </header>
-            <article id='menubar'>aa</article>
+            <article id='menubar'>
+              <div><span class="material-symbols-outlined">home</span></div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div><span class="material-symbols-outlined">trophy</span></div>
+              <div><span class="material-symbols-outlined">build</span></div>
+            </article>
+            <span id='menuOp'>
+              <ul>
+                <li>Verbo to be</li>
+                <li>Numbers</li>
+                <li>Adjectives</li>
+              </ul>
+            </span>
             <main>
               <h2 id='Lets'>Let's build the future together</h2>
               <h3></h3>
               <div className='sumario'>
-                <article>S U M Á R I O</article>
+                <div>S U M Á R I O</div>
                 <ol>
-                  <li>Verbo To-be</li>
-                  <li>Preposições</li>
-                  <li>Números</li>
+                  <li className='capitulos'><a href="#">CAPÍTULO 1</a></li>
+                  <li><a href="#">Verbo To-be</a></li>
+                  <li><a href="#">Preposições</a></li>
+                  <li><a href="#">Números</a></li>
+                  <li className='capitulos'><a href="#">CAPÍTULO 2</a></li>
+                  <li><a href="#">Verbo To-be</a></li>
+                  <li><a href="#">Preposições</a></li>
+                  <li><a href="#">Números</a></li>
+                  <li className='capitulos'><a href="#">CAPÍTULO 3</a></li>
+                  <li><a href="#">Verbo To-be</a></li>
+                  <li><a href="#">Preposições</a></li>
+                  <li><a href="#">Números</a></li>
+                  <li className='capitulos'><a href="#">CAPÍTULO 4</a></li>
+                  <li><a href="#">Verbo To-be</a></li>
+                  <li><a href="#">Preposições</a></li>
+                  <li><a href="#">Números</a></li>
+                  <li className='capitulos'><a href="#">CAPÍTULO 5</a></li>
+                  <li><a href="#">Verbo To-be</a></li>
+                  <li><a href="#">Preposições</a></li>
+                  <li><a href="#">Números</a></li>
                 </ol>
               </div>
+              <div className='linha'></div>
               <div id='conteudo'>
                 <h1 id='cap1'>CAPÍTULO 1</h1>
-                <h2>Verbo To Be</h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, tempore. Praesentium repellat doloribus delectus dolorum <span className='italico'>reprehenderit</span> sapiente et illum, similique sit accusamus <span className='negrito'>libero vel</span> iste ut adipisci explicabo, neque nesciunt?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores numquam repudiandae aut excepturi voluptas? Non dolorem vel consequatur ducimus perspiciatis! Laudantium ut nulla suscipit quae odit, accusamus qui dolores voluptatem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, eos omnis dolores explicabo iure hic error quia quisquam cupiditate porro iusto minima veritatis aspernatur soluta ipsam saepe eius magnam atque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto veniam quas quasi officiis nostrum quia similique ipsa libero aspernatur quidem. Deserunt dignissimos amet porro, suscipit est et harum veniam placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nisi ea ad illo repudiandae est quos incidunt sapiente in! Deleniti porro nemo illo saepe quam quisquam reiciendis. Deserunt, aut suscipit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam tempora, odio itaque ratione velit quia id nostrum, iure, error adipisci numquam ab ut rerum repellat architecto sed in quas aliquam. Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam aliquid architecto autem atque blanditiis vel nostrum, aliquam facilis harum modi earum nemo vitae saepe reiciendis enim, soluta obcaecati. Maiores iusto impedit omnis placeat, amet quaerat inventore, harum laborum aliquam, aliquid ea! Laborum nisi, accusamus, porro eius provident cumque ullam enim quasi ipsum laudantium illo accusantium. Earum aperiam enim odit dolor numquam nesciunt saepe! Quidem, officiis autem? Minima amet, dolor hic quam ullam quisquam quibusdam nobis commodi aliquid excepturi quaerat voluptates quia reiciendis rem placeat culpa mollitia in? Consequuntur accusamus eius voluptatibus, aliquam neque, labore blanditiis voluptate voluptates dolore in amet.</div>
+                <h2>Pronomes Pessoais</h2>
+                <p>Usamos os pronomes pessoais para representar algo ou alguém,               substituindo nomes ou substantivos. Temos dois tipos de personal pronouns: <span className='negrito'>subject and object</span></p>
+                <table>
+                  <tr>
+                    <th>PRONOME</th>
+                    <th>SUBJECT PERSONAL PRONOME</th>
+                    <th>OBJECT PERSONAL PRONOME</th>
+                  </tr>
+                  <tr>
+                    <td>eu</td> <td>I</td> <td>me</td>
+                    
+                  </tr>
+                </table>
+                <div className='linha'></div>
+
+                <h1 id='cap2'>CAPÍTULO 2</h1>
+                <h2>Pronomes Pessoais</h2>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, tempore. Praesentium repellat doloribus delectus dolorum <span className='italico'>reprehenderit</span> sapiente et illum, similique sit accusamus <span className='negrito'>libero vel</span> iste ut adipisci explicabo, neque nesciunt?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores numquam repudiandae aut excepturi voluptas? Non dolorem vel consequatur ducimus perspiciatis! Laudantium ut nulla suscipit quae odit, accusamus qui dolores voluptatem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, eos omnis dolores explicabo iure hic error quia quisquam cupiditate porro iusto minima veritatis aspernatur soluta ipsam saepe eius magnam atque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto veniam quas quasi officiis nostrum quia similique ipsa libero aspernatur quidem. Deserunt dignissimos amet porro, suscipit est et harum veniam placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nisi ea ad illo repudiandae est quos incidunt sapiente in! Deleniti porro nemo illo saepe quam quisquam reiciendis. Deserunt, aut suscipit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam tempora, odio itaque ratione velit quia id nostrum, iure, error adipisci numquam ab ut rerum repellat architecto sed in quas aliquam. Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam aliquid architecto autem atque blanditiis vel nostrum, aliquam facilis harum modi earum nemo vitae saepe reiciendis enim, soluta obcaecati. Maiores iusto impedit omnis placeat, amet quaerat inventore, harum laborum aliquam, aliquid ea! Laborum nisi, accusamus, porro eius provident cumque ullam enim quasi ipsum laudantium illo accusantium. Earum aperiam enim odit dolor numquam nesciunt saepe! Quidem, officiis autem? Minima amet, dolor hic quam ullam quisquam quibusdam nobis commodi aliquid excepturi quaerat voluptates quia reiciendis rem placeat culpa mollitia in? Consequuntur accusamus eius voluptatibus, aliquam neque, labore blanditiis voluptate voluptates dolore in amet.</div>
                 <div className='exercicios'>
             
                 </div>
